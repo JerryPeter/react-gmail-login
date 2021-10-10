05de540c7fff815f828e2897b8849de042f880cd
JerryPeter <jerry.peter@gmail.com>
2021-10-10 21:32:38 +0700

    Edit Readme.md

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  responseGoogle = (response) => {
+    console.log(response);
+    console.log(response.profileObj)
+
+    let msg = `Welcome : ${response.profileObj.name} \nEmail: ${response.profileObj.email}`
+    alert(msg)
+  }
+
+  render() {
+    return (
+      <div>
+        <GoogleLogin 
+        clientId="87013480103-lln71b3skbksg7fgdj44vqqfedea971g.apps.googleusercontent.com"
+        buttonText="Login with Google"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+        cookiePolicy={'single_host_origin'}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
